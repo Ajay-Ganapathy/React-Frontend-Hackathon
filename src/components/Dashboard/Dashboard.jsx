67f01068e7667620046359f2d6f05fb2c1efdc8c
Ajay-Ganapathy <20epci002@skcet.ac.in>
2023-11-24 15:54:57 +0530

    update routes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -17,7 +17,7 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
+import { mainListItems, secondaryListItems } from '../ListItems/ListItems';
 import Chart from '../Charts/Charts';
 import Deposits from '../Deposits/Deposits';
 import Orders from '../Orders/Orders';
