d5cdec14a6b13e318e800803c4d114c58a1008da
Ajay-Ganapathy <82525751+Ajay-Ganapathy@users.noreply.github.com>
2023-11-26 18:00:13 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,7 @@
 import firebase from "firebase";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBwoWtRQWM0z1N0ODYr3GswK7gvKt0SFiE",
-  authDomain: "classroom-clone-9b890.firebaseapp.com",
-  projectId: "classroom-clone-9b890",
-  storageBucket: "classroom-clone-9b890.appspot.com",
-  messagingSenderId: "383818944088",
-  appId: "1:383818944088:web:bc934069eb35c30df3042d",
-  measurementId: "G-F84PC8BNXH"
-};
+
 
 firebase.initializeApp(firebaseConfig)
 
